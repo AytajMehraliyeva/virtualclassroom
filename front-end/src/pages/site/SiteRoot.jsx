7113b903d7e275f1ae09f7e4fef3da3fdf2d81fb
AytajMehraliyeva <tu7lzxxdc@code.edu.az>
2025-07-01 23:44:04 +0400

    end

--- front-end/src/pages/site/SiteRoot.jsx
+++ front-end/src/pages/site/SiteRoot.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../../layouts/Header/Header'
 import {Outlet} from 'react-router'
-import Footer from '../../layouts/Footer/Footer'
 const SiteRoot = () => {
   return (
   <>
